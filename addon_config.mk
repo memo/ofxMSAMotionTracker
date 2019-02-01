8afea163857de339d12c1d91470f0acdc91df110
Memo Akten <memo@memo.tv>
2019-02-01 04:51:38 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSAMotionTracker
+	ADDON_DESCRIPTION = Track motion in video
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMSAMotionTracker
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxOpenCv ofxSimpleGuiToo ofxMSACore
